9aabc25b30bafccedb402b181e0b14a5b9714b06
hubqin <wuhanqinb@gmail.com>
2020-02-02 17:39:03 +0000

    send sms code

---
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\VerificationCodeRequest;
+use Illuminate\Http\Request;
+use Overtrue\EasySms\EasySms;
+use Illuminate\Support\Str;
+
+class VerificationCodesController extends Controller
+{
+    public function store(VerificationCodeRequest $request, EasySms $easySms)
+    {
+        $phone = $request->phone;
+
+        // generate random num with 4 digits
+        $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+
+        if (!app()->environment('production')) {
+            $code = '1234';
+        } else {
+            try {
+                $result = $easySms->send($phone, [
+                    'template' => config('easysms.gateways.aliyun.templates.register'),
+                    'data' => [
+                        'code' => $code,
+                    ]
+                ]);
+            } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+                $message = $exception->getException('aliyun')->getMessage();
+                abort(500, $message ?: "短信发送异常");
+            }
+        }
+
+        $key = 'verificationCode_' . Str::random(15);
+        $expiredAt = now()->addMinutes(5);
+        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+
+        return response()->json([
+            'key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
+        ])->setStatusCode(201);
+    }
+}
